a0aebcd3d53df84a2b8be1aaff803ce19d1480b0
BigCubeCat <>
2021-11-30 20:06:10 +0700

    first commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+#include "mainwindow.h"
 
-using namespace std;
+#include <QApplication>
 
-int main() {
-    cout << "hi\n";
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
