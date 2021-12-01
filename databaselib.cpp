26a4a6e92e3f20e978bc905571cfe2af8e6b1a34
Kirill Parkhomenko <74421236+KXRXH@users.noreply.github.com>
2021-12-01 18:15:02 +0700

    new func's

--- databaselib.cpp
+++ databaselib.cpp
@@ -1,6 +1,12 @@
 #include "databaselib.h"
-#include <QDebug>
 
+//PRIVATE:
+int DataBaseLib::get_table_columns_count(QSqlQuery dbquery) {
+    QSqlRecord rec = dbquery.record();
+    return rec.count();
+}
+
+//PABLIC:
 bool DataBaseLib::connect_to_base() {
     db.setDatabaseName("main.db");
     return db.open();
@@ -11,20 +17,29 @@ QList<QString> DataBaseLib::get_database_tables() {
 }
 
 std::vector<std::vector<QString>> DataBaseLib::db_select(QString _request) {
+  std::vector<std::vector<QString>> all_rows_vector;
+
   QSqlQuery query(db);
   query.exec(_request);
-  std::vector<std::vector<QString>> all_rows_vector;
+
+  int table_columns = get_table_columns_count(query);
+
   while (query.next()) {
       std::vector<QString> row_vector;
-      for (int i = 0; i < 7; ++i) {
+      for (int i = 0; i < table_columns; ++i) {
           row_vector.push_back(query.value(i).toString());
       }
       all_rows_vector.push_back(row_vector);
   }
+  qDebug() << all_rows_vector << table_columns;
   return all_rows_vector;
 }
 
-std::vector<std::vector<QString>> DataBaseLib::get_table(QString table_name) {
-    std::vector<std::vector<QString>> data;
-    return db_select("SELECT * FROM " + table_name);
+void DataBaseLib::delete_row_by_id(QString table_name, int id) {
+    QSqlQuery query_delete(db);
+    query_delete.prepare("DELETE FROM " + table_name +
+                         " WHERE id = " + QString::number(id));
+    query_delete.exec();
 }
+
+
--- databaselib.h
+++ databaselib.h
@@ -1,22 +1,24 @@
 #ifndef DATABASELIB_H
 #define DATABASELIB_H
 #include <QtSql/QSqlDatabase>
-#include <QtSql/QSqlRecord>
 #include <QtSql/QSqlQuery>
 #include <QList>
 #include <vector>
 #include <QDebug>
+#include <QtSql/QSqlRecord>
 
 class DataBaseLib
 {
 private:
     QSqlDatabase db;
+    int get_table_columns_count(QSqlQuery dbquery);
 public:
     DataBaseLib(QSqlDatabase database) { db = database; };
     bool connect_to_base();
     QList<QString> get_database_tables();
-    std::vector<std::vector<QString>> get_table(QString table_name);
     std::vector<std::vector<QString>> db_select(QString _request);
+    void delete_row_by_id(QString table_name, int id);
+    // void insert_row_to_table(QString table_name, std::vector<std::vector<QString>)
 };
 
 #endif // DATABASELIB_H
